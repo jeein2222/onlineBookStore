60a027aefca72081e6422085cf82b18ab1584442
jeein2222 <jiinbag057@gmail.com>
2022-04-07 14:35:45 +0900

    BookRepository

---
+++ ecommerce/persistence/BookRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.ecommerce.persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.ecommerce.model.BookEntity;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity,String>{
+	List<BookEntity> findByUserId(String userId);
+	List<BookEntity> findByTitle(String title);
+}
